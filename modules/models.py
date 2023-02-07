ddd8db8c1bbd641329eb771fb6fba1b2649f4ff7
yasaminborhani <77745618+yasaminborhani@users.noreply.github.com>
2023-02-07 21:19:43 +0330

    Update models.py

--- modules/models.py
+++ modules/models.py
@@ -279,8 +279,8 @@ def Backbone(backbone_type='ResNet50', use_pretrain=True):
         elif backbone_type == 'rvit':
             print('input shape chie', x.shape[1:])
             inp = tf.keras.layers.Input(x.shape[1:])
-            x = rvit_backbone()(inp)
-            extractor = tf.keras.Model(inp, x, name='rvit_model')
+            layer = rvit_backbone()(inp)
+            extractor = tf.keras.Model(inp, layer, name='rvit_model')
             preprocess = tf.keras.applications.resnet.preprocess_input
 
         else:
